d05e9f70a5643b085633b17495a63b31a14943d9
John Harmon <johnharmon805@gmail.com>
2020-03-28 21:55:10 -0700

    firebase ready

--- train.js
+++ train.js
@@ -1,17 +1,38 @@
-$("#add-train-btn").on("click", function(event) {
-            event.preventDefault();
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+     apiKey: "AIzaSyByNEozd8w1LihHe0vjBrtWc3NwxcCC-cI",
+     authDomain: "train-schedule-c547f.firebaseapp.com",
+     databaseURL: "https://train-schedule-c547f.firebaseio.com",
+     projectId: "train-schedule-c547f",
+     storageBucket: "train-schedule-c547f.appspot.com",
+     messagingSenderId: "123784825277",
+     appId: "1:123784825277:web:4d3be48ec1612d267a2ad4"
+ };
+ // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
 
-            // Grabs user input
-            var name = $("#train-name").val().trim();
-            var place = $("#train-destination").val().trim();
-            var startTime = $("#first-train-time").val().trim();
-            var frequency = $("#train-frequency").val().trim();
+ $("#add-train-btn").on("click", function(event) {
+             event.preventDefault();
 
-            // Creates local "temporary" object for holding train data
-            var newTrain = {
-                name: name,
-                destination: place,
-                firstTrainTime: startTime,
-                trainFrequency: frequency
-            };
-        }
+             // Grabs user input
+             var name = $("#train-name").val().trim();
+             var place = $("#train-destination").val().trim();
+             var startTime = $("#first-train-time").val().trim();
+             var frequency = $("#train-frequency").val().trim();
+
+             // Creates local "temporary" object for holding train data
+             var newTrain = {
+                 name: name,
+                 destination: place,
+                 firstTrainTime: startTime,
+                 trainFrequency: frequency
+             };
+
+
+
+             database.ref().push(newTrain);
+
+             console.log(newTrain.name);
+             console.log(newTrain.destination);
+             console.log(newTrain.firstTrainTime);
+             console.log(newTrain.trainFrequency);
